6944ac22c62a43ea2837b88f19817aee854a49b0
diondevgitco <rihabesh05@gmail.com>
2026-05-02 12:14:16 +0100

    Fix Express 5 wildcard route

--- server.js
+++ server.js
@@ -5,30 +5,16 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "public")));
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, "0.0.0.0", () => {
-  console.log("Server running on port " + PORT);
-});const express = require("express");
-const path = require("path");
-
-const app = express();
-
-// static
-app.use(express.static(path.join(__dirname, "public")));
-
-// test route (باش نتأكدو)
 app.get("/test", (req, res) => {
   res.send("OK WORKING");
 });
 
-// fallback (مهم بزاف)
-app.get("*", (req, res) => {
+app.get("/{*path}", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
-  console.log("Server running on port " + PORT);
+  console.log(`Server running on port ${PORT}`);
 });
